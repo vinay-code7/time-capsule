afaf221ebd1410da943da8721d675e4f6bceda61
vinaycode7@gmail.com <vinaycode7@gmail.com>
2023-08-19 00:09:39 +0530

    added flask starter code

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+# app configuration
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
